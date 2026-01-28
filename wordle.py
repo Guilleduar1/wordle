b34de529ed296f61f3f2e848f189c3d9bb87657c
h3r3tic <guilledu97@gmail.com>
2026-01-28 14:57:21 -0300

    se acaban de definir la cantidad de intentos

--- wordle.py
+++ wordle.py
@@ -12,7 +12,15 @@ def verificador_palabra(palabra_ingresada, palabra_secreta):
             letras_verificadas.append(f"({palabra_ingresada[i]})")
         else:
             letras_verificadas.append(palabra_ingresada[i])
-    
     return letras_verificadas
 
 verificador_palabra('holas', 'calor')
+
+# definir la cantidad de intentos = variable
+intentos = 0
+
+while intentos < 6:
+    print(f"te quedan {6 - intentos} intentos")
+    intentos = intentos + 1
+    palabra_ingresada = input("Ingrese una palabra")
+    print(f"la palabra ingresada es: {palabra_ingresada}")
